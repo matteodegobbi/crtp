0648b8a03641ab933f048e3c7ec7f66572e44a8f
Andrea Rigoni <andrea.rgn@gmail.com>
2021-10-14 07:20:18 +0200

    fix sobel

--- src/lab3/GradientNonOptimized.c
+++ src/lab3/GradientNonOptimized.c
@@ -3,9 +3,9 @@
 #define THRESHOLD 120
 /* Sobel matrixes */
 static const int GX[3][3] = { 
-        -1, 0, 1,
-        -1, 0, 2,
-        -1, 0, 1
+        1, 0, -1,
+        2, 0, -2,
+        1, 0, -1
     };
     
 static const int GY[3][3] = { 
